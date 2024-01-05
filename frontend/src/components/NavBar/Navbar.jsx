ea9080453e24fdfd7fe9ff24b59897340ff6287c
IsHaNinfo <ishaninfo56@gmail.com>
2024-01-05 16:29:40 +0530

    add the stars

--- frontend/src/components/NavBar/Navbar.jsx
+++ frontend/src/components/NavBar/Navbar.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import "./navbar.css"
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import Logo from "../../../image/Logo.png"
-const Navbar = () => {
+const Navbars = () => {
   return (
     <div>
-         <nav className="navbar navbar-light bg-light shadow">
+ <Navbar expand="lg" className="nav">
+        <Container className='ml-0'>
+        <Navbar.Brand href="#home" className="d-flex align-items-center">
+            <img
+              src={Logo}
+              alt="Lenzz Rating system logo"
+              height="40" // Set the desired height
+              className="d-inline-block align-top"
+            />
+            <span className="ms-4">Lenzz Rating System</span>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              {/* Add your navigation links or components here */}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+         {/* <nav className="navbar navbar-light bg-light shadow">
             <div className="container-fluid">
                 <div className="d-flex justify-content-between align-items-center w-100">
                     <div className="imageDiv">
@@ -13,9 +36,9 @@ const Navbar = () => {
                     </div>
                 </div>
             </div>
-        </nav>
+        </nav> */}
     </div>
   )
 }
 
-export default Navbar
+export default Navbars
--- frontend/src/pages/Rating/Rating.jsx
+++ frontend/src/pages/Rating/Rating.jsx
@@ -1,10 +1,63 @@
 import React from 'react'
-import Navbar from '../../components/NavBar/Navbar'
-
+import Navbars from '../../components/NavBar/Navbar'
+import Carousel from 'react-bootstrap/Carousel';
+import Modals from '../../components/Modal/Modal';
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Men from "../../../image/Men.png"
 const Rating = () => {
-  return (
-    <div><Navbar/></div>
-  )
-}
+  const [index, setIndex] = useState(0);
 
+  const modalData = [
+    {
+      firstName: "John",
+      LastName: "Doe",
+      job: "Software Engineer",
+      description: "A software engineer designs, develops, and maintains software applications, using programming skills and problem-solving abilities. They work collaboratively to create efficient and functional solutions, contributing to the entire software development lifecycle across various industries.",
+      image: Men,
+ 
+    },
+    {
+      firstName: "John",
+      LastName: "Doe",
+      job: "Graphic Designer",
+      description: "A software engineer designs, develops, and maintains software applications, using programming skills and problem-solving abilities. They work collaboratively to create efficient and functional solutions, contributing to the entire software development lifecycle across various industries.",
+      image: Men,
+    },
+    {
+      firstName: "John",
+      LastName: "Doe",
+      job: "Marketing Specialist",
+      description: "A software engineer designs, develops, and maintains software applications, using programming skills and problem-solving abilities. They work collaboratively to create efficient and functional solutions, contributing to the entire software development lifecycle across various industries.",
+      image: Men,
+    },
+    {
+      firstName: "kasun",
+      LastName: "Doe",
+      job: "Marketing Specialist",
+      description: "A software engineer designs, develops, and maintains software applications, using programming skills and problem-solving abilities. They work collaboratively to create efficient and functional solutions, contributing to the entire software development lifecycle across various industries.",
+      image: Men,
+    },
+  ];
+
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <div>
+      <Navbars />
+      <div className='d-flex justify-content-center align-items-center vh-100'>
+      <Carousel activeIndex={index} onSelect={handleSelect} >
+        {modalData.map((data, i) => (
+          <Carousel.Item key={i}>
+            <Modals data={data} />
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    
+      </div>
+    </div>
+  );
+};
 export default Rating
