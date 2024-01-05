317133b708b1b1ef2aa9d32548ac2403f7805f22
UserSampath <nalakasampathsmp@gmail.com>
2024-01-05 10:30:10 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import "./app.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from "./pages/signIn/SignIn";
+import SignUp from "./pages/signUp/SignUp";
+import Home from "./pages/home/Home";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<SignIn />}></Route>
+        <Route path="/register" element={<SignUp />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
