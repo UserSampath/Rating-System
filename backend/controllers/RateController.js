06d2bf97a21db1cb926b04808eba74283c476eed
UserSampath <nalakasampathsmp@gmail.com>
2024-01-07 02:34:01 +0530

    delete rate user controller

--- backend/controllers/RateController.js
+++ backend/controllers/RateController.js
@@ -83,31 +83,11 @@ const GetRateUser = async (req, res) => {
     }
 };
 
-const rateUser = async (req, res) => {
-    const { firstName, lastName, Job, Description, Rate, Image } = req.body;
-
-    try {
-        const updatedRateUser = await RateuserModel.findByIdAndUpdate(
-            userId,
-            { firstName, lastName, Job, Description, Rate, Image },
-            { new: true, runValidators: true }
-        );
-
-        if (!updatedRateUser) {
-            throw Error("User not found");
-        }
-
-        res.status(200).json({ message: "User updated successfully", updatedRateUser });
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
-};
-
 
 module.exports = {
     RateUserAdd,
     DeleteRateUser,
     UpdateRateUser,
     GetRateUser,
-    rateUser
+    
 }
--- backend/routes/RateUser.js
+++ backend/routes/RateUser.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { RateUserAdd, DeleteRateUser, UpdateRateUser, GetRateUser, rateUser } = require("../controllers/RateController") ;
+const { RateUserAdd, DeleteRateUser, UpdateRateUser, GetRateUser } = require("../controllers/RateController") ;
 
 router.post("/addRateUser", RateUserAdd);
 router.delete ("/deleteRateUser/:id",DeleteRateUser);
 router.put("/UpdateRateUser/:id", UpdateRateUser);
-router.put("/rateUser", rateUser);
+
 router.get ("/getRateUser/:id",GetRateUser);
 module.exports = router;
